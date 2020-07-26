46b0964c686e98c3f924d4400653ff7f5f71c824
WaterNguyen96 <nnthuynguyen5196@gmail.com>
2020-07-26 22:47:03 +0700

    - rm files- add fira-code font- change vscode extensions- add alias & PATH in zshrc

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 install() {
-	curl -Lso /tmp/mono.zip https://github.com/JetBrains/JetBrainsMono/releases/download/v1.0.6/JetBrainsMono-1.0.6.zip
-	unzip -j /tmp/mono.zip '*/ttf/*' -d "$1"
-
 	curl -Lso /tmp/hack.zip https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
 	unzip -j /tmp/hack.zip -d "$1"
 }
@@ -10,7 +7,7 @@ install() {
 if [ "$(uname -s)" = "Darwin" ]; then
 	if command -v brew >/dev/null 2>&1; then
 		brew tap homebrew/cask-fonts
-		brew cask install font-jetbrains-mono font-hack
+		brew cask install font-hack font-fira-code
 	else
 		install ~/Library/Fonts
 	fi
@@ -18,4 +15,5 @@ else
 	mkdir -p ~/.local/share/fonts/
 	install ~/.local/share/fonts/
 	fc-cache -fv
+	sudo add-apt-repository universe && sudo apt install fonts-firacode
 fi
