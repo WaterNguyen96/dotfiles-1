bbb7bb0ef723cc8f911737a8b0a928a79619e42a
WaterNguyen96 <nnthuynguyen5196@gmail.com>
2020-07-26 22:47:03 +0700

    - rm files - add fira-code font - change vscode extensions - add alias & PATH in zshrc

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,16 @@
 #!/bin/sh
 export PATH="$PATH:$DOTFILES/bin:$HOME/.bin"
+export PATH="$PATH:/usr/bin/robomongo/bin"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH=$HOME/flutter/bin:$PATH
+export PATH=$HOME/dart/bin:$PATH
+
+export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
+
+export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_221
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
